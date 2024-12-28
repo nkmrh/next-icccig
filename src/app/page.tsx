476f14771ad637a0e874a3e8677932a8d7a52026
Hajime Nakamura <5644420+nkmrh@users.noreply.github.com>
2024-12-28 20:22:15 +0900

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div>
-        <Link href="https://github.com/nkmrh/next-icccig">next-icccig</Link>
-        <h2>
+        <Link className='text-2xl text-stone-900' href="https://github.com/nkmrh/next-icccig">next-icccig</Link>
+        <h2 className='text-base text-stone-900'>
           This site generate images of itunes card content code. (for readable
           OCR)
         </h2>
-        <Link href="/6MPAY3FFELPE">e.g. https://next-icccig.vercel.app/6MPAY3FFELPE</Link>
+        <Link className='text-sm text-stone-900' href="/6MPAY3FFELPE">e.g. https://next-icccig.vercel.app/<b>6MPAY3FFELPE</b></Link>
       </div>
     </div>
   );
