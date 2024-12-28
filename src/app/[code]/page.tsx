3c1fa507129f6cc206d9ea57b2c6b6f3e3a57e4e
Hajime Nakamura <5644420+nkmrh@users.noreply.github.com>
2024-12-28 16:21:29 +0900

    implement

--- src/app/[code]/page.tsx
+++ src/app/[code]/page.tsx
@@ -1,10 +1,21 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const page = async ({ params }: { params: Promise<{ code: string }> }) => {
   const { code } = await params;
-  const chars = code.split('')
-  console.log()
-  return (<Image src="/font/A.jpg" width={100} height={100} alt=""/>)
+  const chars = code.split("");
+  const srcs = chars.map((c) => `/font/${c}.jpg`);
+  console.log(srcs);
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex border-solid border-2 border-black">
+        <div className="flex my-7 mx-7">
+          {srcs.map((s, i) => (
+            <Image key={i} src={s} width={15} height={21} alt="" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
