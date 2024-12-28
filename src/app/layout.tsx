360a185eef953fe93300c72a02f3edcf923eee14
Hajime Nakamura <5644420+nkmrh@users.noreply.github.com>
2024-12-28 18:51:39 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"\
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
